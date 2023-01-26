dfaa4e6512749287f4707f897aca6042bce2e1d6
Mr. Lawrence <lbajoie34@gmail.com>
2023-01-26 17:14:55 -0600

    to help Lauren with fractions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Your code here!
+import React ,{useState} from 'react';
+import ReactDOM from 'react-dom';
+function Counter () {
+const [count, setCount] = useState(0);
+const increment = () => {
+    setCount(count + 2/4)
+}  
+    return(
+        <div id='container'>
+            <div id='navbar'>
+                fractionToDecimalCounter.js
+            </div>
+            <p id='title'>FRACTION TO DECIMAL CONVERTeR</p>
+            <div id='counter'>
+                <h1>{count}</h1>
+                <button id='button' onClick={increment}>CONVeRT</button>
+            </div>
+        </div>
+    ) 
+}
+console.log('helloWorld')
+//console.log(setCount)
+ReactDOM.render(<Counter />, document.getElementById('app'));
+//ReactDOM.render(increment(), document.getElementById('button'));
